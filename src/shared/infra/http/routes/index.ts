cacb13c56de336415148bdbd0f7cc3289c152e66
Lula Crackudo <lucasfonsecard@icloud.com>
2021-03-26 12:38:41 -0300

    add password routes&controllers

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import appointmentsRouter from '@appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@users/infra/http/routes/users.routes';
 import sessionsRouter from '@users/infra/http/routes/sessions.routes';
+import passwordRouter from '@users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
