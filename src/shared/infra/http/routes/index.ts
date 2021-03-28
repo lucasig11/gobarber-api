da96babb7ad2f88f3d843ce592e7716bdfad52ac
Lula Crackudo <lucasfonsecard@icloud.com>
2021-03-28 05:49:37 -0300

    update shared routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
 import appointmentsRouter from '@appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@appointments/infra/http/routes/providers.routes';
+
 import usersRouter from '@users/infra/http/routes/users.routes';
 import profileRouter from '@users/infra/http/routes/profile.routes';
 import sessionsRouter from '@users/infra/http/routes/sessions.routes';
@@ -9,6 +11,7 @@ import passwordRouter from '@users/infra/http/routes/password.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/profile', profileRouter);
 routes.use('/sessions', sessionsRouter);
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,12 @@
-import 'reflect-metadata';
-
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
 import cors from 'cors';
 
-import routes from './routes';
-import uploadConfig from '@config/upload';
+import 'reflect-metadata';
+import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -19,20 +18,21 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
 
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal server error.',
     });
-  }
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error.',
-  });
-});
+  },
+);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}.`);
