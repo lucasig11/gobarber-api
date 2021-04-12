8ac9a10db7b33404cded1a9f213372e27cc0301e
Lula Crackudo <lucasfonsecard@icloud.com>
2021-04-12 13:57:55 -0300

    Update appointments module to save customer id
    The customer id also needed to be saved, instead of only the provider_id.

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,36 +2,43 @@ import { startOfHour } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 
-import Appointment from '../infra/typeorm/entities/Appointments'
 import IAppointmentsRepository from '@appointments/repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointments';
 
 interface IRequest {
-  provider_id: string,
-  date: Date,
+  provider_id: string;
+  user_id: string;
+  date: Date;
 }
 
 @injectable()
 class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository){}
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
 
-  public async execute({ date, provider_id }: IRequest): Promise <Appointment> {
-
+  public async execute({
+    date,
+    user_id,
+    provider_id,
+  }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked.');
     }
-
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
-    return appointment
+    return appointment;
   }
 }
 
