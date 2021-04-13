83d7ae0ffaf10aa79bf8811600d66e62d215b5d3
Lula Crackudo <lucasfonsecard@icloud.com>
2021-04-13 09:59:28 -0300

    register notification's repository singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,4 @@
-import {container} from 'tsyringe';
+import { container } from 'tsyringe';
 
 import '@modules/users/providers';
 import './providers';
@@ -12,6 +12,22 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
-container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository);
-container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
-container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository);
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
+);
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
+);
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
+);
