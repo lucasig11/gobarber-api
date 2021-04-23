946b1cb3374136a1e175878f4b3c62f1733befec
lucas <lucasfonsecard@icloud.com>
2021-04-23 00:56:01 -0300

    update import order

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import 'express-async-errors';
 import 'dotenv';
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
@@ -9,9 +11,6 @@ import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
-
-import '@shared/infra/typeorm';
-import '@shared/container';
 
 const app = express();
 const port = 3333;
