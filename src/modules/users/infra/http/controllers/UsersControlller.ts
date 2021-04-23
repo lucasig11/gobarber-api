48449c244f0cf95419736149fb5aa4f86e9a8a61
lucas <lucasfonsecard@icloud.com>
2021-04-23 08:59:29 -0300

    update imports

--- src/modules/users/infra/http/controllers/UsersControlller.ts
+++ src/modules/users/infra/http/controllers/UsersControlller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@users/services/CreateUserService';
-import { classToClass } from 'class-transformer';
 
 export default class UsersControllers {
   public async create(request: Request, response: Response): Promise<Response> {
