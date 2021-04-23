24e98448501391288ced7a692347f1baf6a10b6e
lucas <lucasfonsecard@icloud.com>
2021-04-23 00:56:08 -0300

    refactor containers

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -1,8 +1,16 @@
-import { container } from 'tsyringe'
-import EtherealMailProvider from './implementations/EtherealMailProvider'
-import SESMailProvider from './implementations/SESMailProvider'
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
-export default {
-    ethereal: container.resolve(EtherealMailProvider),
-    ses: container.resolve(SESMailProvider)
-}
+import IMailProvider from './models/IMailProvider';
+import EtherealMailProvider from './implementations/EtherealMailProvider';
+import SESMailProvider from './implementations/SESMailProvider';
+
+const providers = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  providers[mailConfig.driver],
+);
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,25 +1,3 @@
-import { container } from 'tsyringe';
-
-import mailConfig from '@config/mail';
-
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProvider/models/IMailProvider';
-import mailProviders from './MailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerInstance<IStorageProvider>(
-  'StorageProvider',
-  new DiskStorageProvider(),
-);
-container.registerInstance<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  new HandlebarsMailTemplateProvider(),
-);
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailProviders[mailConfig.driver],
-);
+import './MailTemplateProvider';
+import './MailProvider';
+import './StorageProvider';
