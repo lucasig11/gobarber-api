23dcce2535d143ee7b7c01c4a4be2861fd1e2a2a
lucas <lucasfonsecard@icloud.com>
2021-04-28 05:45:41 -0300

    fix reset password controller little bug

--- src/modules/users/infra/http/controllers/ResetPasswordController.ts
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -6,7 +6,7 @@ import ResetPasswordService from '@users/services/ResetPasswordService';
 export default class ForgotPasswordController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { password, token } = request.body;
-
+    console.log(token);
     const resetPassword = container.resolve(ResetPasswordService);
 
     await resetPassword.execute({
@@ -14,6 +14,6 @@ export default class ForgotPasswordController {
       password,
     });
 
-    return response.status(204);
+    return response.status(204).json();
   }
 }
