cf0a2a9914b747532adfef89c7de02712ba9362a
lucas <lucasfonsecard@icloud.com>
2021-04-28 00:54:20 -0300

    fix date checking

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,4 @@
-import { getDate, getDaysInMonth } from 'date-fns';
+import { endOfDay, getDate, getDaysInMonth, isAfter } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -40,11 +40,17 @@ export default class ListProvidersMonthAvailabilityService {
     );
 
     const availability = monthArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(
         appointment => getDate(appointment.date) === day,
       );
 
-      return { day, available: appointmentsInDay.length < 10 };
+      return {
+        day,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+      };
     });
 
     return availability;
